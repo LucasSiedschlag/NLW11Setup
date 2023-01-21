202df4ed235615eeb410bc564a5c9202e4ad5705
LucasSiedschlag <lucas.sied@gmail.com>
2023-01-20 21:51:42 -0300

    save data on localStorage

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 const form = document.querySelector("form")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-03"],
-  water: ["01-01", "01-02", "01-04", "01-05"],
-  gym: ["01-01", "01-02"],
-  sleep: ["01-01", "01-03", "01-04", "01-05"],
-  food: ["01-02", "01-03", "01-04", "01-06"]
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, 5)
+
+  const dayExists = nlwSetup.dayExists(today)
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+  nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
+function save() {
+  localStorage.setItem("StorageHabits", JSON.stringify(nlwSetup.data))
+}
+
+// const data = {
+//   run: ["01-01", "01-02", "01-03"],
+//   water: ["01-01", "01-02", "01-04", "01-05"],
+//   gym: ["01-01", "01-02"],
+//   sleep: ["01-01", "01-03", "01-04", "01-05"],
+//   food: ["01-02", "01-03", "01-04", "01-06"]
+// }
+
+const data = JSON.parse(localStorage.getItem("StorageHabits"))
+if (data != null) {
+  nlwSetup.setData(data)
+}
 nlwSetup.load()
